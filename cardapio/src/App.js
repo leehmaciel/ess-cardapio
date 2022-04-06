2987224b4c0ce7503f06d9b4316f3c8eb31652ad
Leticia Maciel <leticia.maciel@liferay.com>
2022-04-06 12:46:12 -0300

    Add header and categories menu

---
+++ cardapio/src/App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import Menu from "./Menu";
+import Categories from "./Categories";
+import './App.css';
+import items from "./data";
+import logo from "./logo.svg";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+const App = () => {
+  
+    const [menuItems, setMenuItems] = useState(items);
+    const [activeCategory, setActiveCategory] = useState("");
+    const [categories, setCategories] = useState(allCategories);
+    
+    const filterItems = (category) => {
+      setActiveCategory (category);
+      if (category === "all") {
+        setMenuItems(items);
+        return;
+      }
+      const newItems = items.filter((item) => item.category === category);
+      setMenuItems(newItems);
+    };
+
+    return (
+    <main>
+      <section className="menu section"> 
+        <div className="title">
+          <img src={logo} className="logo" />
+          <h2> Menu List </h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} activeCategory={activeCategory} filterItems={filterItems} /> 
+      </section>
+    </main>
+  );
+};
+
+export default App;
