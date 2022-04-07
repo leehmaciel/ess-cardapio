5f4ecd227ebc31398bb074d91b20e6b7be08c671
Leticia Maciel <leticia.maciel@liferay.com>
2022-04-07 15:13:29 -0300

    Create Routes to Add and Edit pages

--- projeto/client/src/App.js
+++ projeto/client/src/App.js
@@ -1,41 +1,20 @@
 import React, {useState} from 'react';
-import Menu from "./Menu";
-import Categories from "./Categories";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./Home";
+import Add from "./Add";
+import Edit from "./Edit";
 import './App.css';
-import items from "./data";
-import logo from "./logo.svg";
 
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
-
-const App = () => {
-  
-    const [menuItems, setMenuItems] = useState(items);
-    const [activeCategory, setActiveCategory] = useState("");
-    const [categories, setCategories] = useState(allCategories);
-    
-    const filterItems = (category) => {
-      setActiveCategory (category);
-      if (category === "all") {
-        setMenuItems(items);
-        return;
-      }
-      const newItems = items.filter((item) => item.category === category);
-      setMenuItems(newItems);
-    };
-
-    return (
-    <main>
-      <section className="menu section"> 
-        <div className="title">
-          <img src={logo} className="logo" />
-          <h2> Menu List </h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} activeCategory={activeCategory} filterItems={filterItems} /> 
-        <Menu items={menuItems} />
-      </section>
-    </main>
+function App() {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
