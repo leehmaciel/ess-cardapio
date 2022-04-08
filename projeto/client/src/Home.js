3cd1f790503b1ea5419a495b6ff417c51db82800
Leticia Maciel <leticia.maciel@liferay.com>
2022-04-07 22:37:34 -0300

    Create Home and Edit pages

---
+++ projeto/client/src/Home.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import './App.css';
+import items from "./data";
+import logo from "./logo.svg";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+const Home = () => {
+
+    let navigate = useNavigate ();
+
+    const [menuItems, setMenuItems] = useState(items);
+    const [activeCategory, setActiveCategory] = useState("");
+    const [categories, setCategories] = useState(allCategories);
+    
+    const filterItems = (category) => {
+      setActiveCategory (category);
+      if (category === "all") {
+        setMenuItems(items);
+        return;
+      }
+      const newItems = items.filter((item) => item.category === category);
+      setMenuItems(newItems);
+    };
+
+    return (
+    <main>
+      <section className="menu section"> 
+
+        <div className="title">
+          <img src={logo} className="logo" />
+          <h2> Menu List </h2>
+          <div className="underline"></div>
+        </div>
+
+        <div className="navigation">
+          <button onClick={() => {navigate("/add");}}>Add</button>
+          <button onClick={() => {navigate("/edit");}}>Edit</button>
+        </div>
+
+        <Categories categories={categories} activeCategory={activeCategory} filterItems={filterItems} /> 
+        <Menu items={menuItems} />
+        
+      </section>
+    </main>
+  );
+};
+
+export default Home;
